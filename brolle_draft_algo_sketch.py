e949885963e9e083b644c02215d68e4c601494b1
Alvin Svanberg <alvin.svanberg@student.tabyenskilda.se>
2025-01-10 18:37:51 +0100

    more sketching

--- brolle_draft_algo_sketch.py
+++ brolle_draft_algo_sketch.py
@@ -3,19 +3,19 @@ maps = [
         "name": "snake prairie",
         "mode": "bounty",
         "best_categories": ["tank", "assassin"],
-        "range_rating": 0
+        "range_rating": None
     },
     {
         "name": "safe zone",
         "mode": "heist",
         "best_categories": ["tank_counter/DPS", "sniper", "tank"],
-        "range_rating": 80
+        "range_rating": (693-86)/693    #andel tiles som är öppen map eller buskar
     },
     {
         "name": "shooting star",
         "mode": "bounty",
         "best_categories": ["sniper", "assassin", "thrower"],
-        "range_rating": 100
+        "range_rating": (693-72)/693    #andel tiles som är öppen map eller buskar
     }
 ]
 
