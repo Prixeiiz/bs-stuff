8848f8fae6831ded442edf7d907479c50257293d
Alvin Svanberg <alvin.svanberg@student.tabyenskilda.se>
2025-01-10 17:40:43 +0100

    sketch

---
+++ brolle_draft_algo_sketch.py
@@ -0,0 +1,36 @@
+maps = [
+    {
+        "name": "snake prairie",
+        "mode": "bounty",
+        "best_picks": ["shelly", "bo", "buzz"],
+    },
+    {
+        "name": "safe zone",
+        "mode": "heist",
+        "best_picks": ["colette", "angelo", "nani"]
+    }
+]
+
+
+brawlers = [
+    {
+        "name": "shelly",
+        "categories": ["tank_counter/DPS", "tank"],
+        "range": "short"
+    },
+    {
+        "name": "nita",
+        "categories": ["tank_counter/DPS"],
+        "range": "mid"
+    },
+    {
+        "name": "colt",
+        "categories": ["sharpshooter", "tank_counter/DPS"],
+        "range": "long"
+    },
+    {
+        "name": "bull",
+        "categories": ["tank"],
+        "range": "short"
+    }
+]
