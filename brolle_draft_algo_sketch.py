fbeb8e1e2dcef378acdb76eb66ab8d1c926bfe18
Alvin Svanberg <alvin.svanberg@student.tabyenskilda.se>
2025-01-15 21:36:26 +0100

    data structure

--- brolle_draft_algo_sketch.py
+++ brolle_draft_algo_sketch.py
@@ -20,6 +20,16 @@ maps = [
 ]
 
 
+class Brawler:
+    def __init__(self, brawl_name, categories, range, hp):
+        self.brawl_name = brawl_name
+        self.categories = categories
+        self.range = range
+        self.hp = hp
+
+
+
+'''
 brawlers = [
     {
         "name": "shelly",
@@ -41,4 +51,41 @@ brawlers = [
         "categories": ["tank"],
         "range": "short"
     }
-]
+]'''
+
+#en typ av datastruktur, jobbigt med lista pga ordnade element?
+brawlers = [
+            Brawler("tick", ["thrower"], "long", 2200),
+            Brawler("frank", ["tank"], "short", 6700),
+            Brawler("barley",["thrower"], "mid", 2400),
+            Brawler("brock", ["sniper", "tank counter/DPS"], "long", 2400),
+            Brawler("byron", ["sniper", "support"], "long", 2400),
+            Brawler("crow", ["assassin"], "mid", 2400),
+            Brawler("nani", ["sniper", "tank counter/DPS"], "long", 2400),
+            Brawler("piper", ["sniper", "tank counter/DPS"], "long", 2300),
+            Brawler("primo", ["tank"], "short", 6000),
+            Brawler("draco", ["tank"], "short", 5500)
+            ]
+
+
+#bättre med dict?
+brawlers_test = {
+    "tick": Brawler("TICK", ["thrower"], "long", 2200),
+    "frank": Brawler("FRANK", ["tank"], "short", 6700),
+    "byron": Brawler("BYRON",["sniper", "support"], "long", 2400)
+}
+
+print(brawlers_test["tick"].hp)
+print(brawlers[9].brawl_name)
+
+def hp_rating(brawler_list):        #räknar alla hp som lvl 1 brawlers
+    if len(brawler_list) == 1:
+        min_sum = brawlers[0].hp                        #tick (lägst)
+        max_sum = brawlers[1].hp                        #franks hp (högst)
+        #skala in värdena:
+    elif len(brawler_list) == 2:
+        minsum = brawlers[0].hp + brawlers[7].hp        #ticks + pipers hp
+        maxsum = brawlers[1].hp + brawlers[8].hp        #franks + primos hp
+    elif len(brawler_list) == 3:
+        sum_min = brawlers[0].hp + brawlers[7].hp + brawlers[6].hp  #ticks + pipers + nanis hp
+        sum_max = brawlers[1].hp + brawlers[8].hp + brawlers[9].hp      #franks + primos + dracos hp (högst o fallande)
