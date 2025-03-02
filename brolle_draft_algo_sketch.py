ee52fd8f4397cff0796f6b2f7887b8a21d731d7f
Alvin Svanberg <alvin.svanberg@student.tabyenskilda.se>
2025-03-02 19:58:31 +0100

    structure

--- brolle_draft_algo_sketch.py
+++ brolle_draft_algo_sketch.py
@@ -1,91 +1,54 @@
-maps = [
-    {
-        "name": "snake prairie",
-        "mode": "bounty",
-        "best_categories": ["tank", "assassin"],
-        "range_rating": None
-    },
-    {
-        "name": "safe zone",
-        "mode": "heist",
-        "best_categories": ["tank_counter/DPS", "sniper", "tank"],
-        "range_rating": (693-86)/693    #andel tiles som är öppen map eller buskar
-    },
-    {
-        "name": "shooting star",
-        "mode": "bounty",
-        "best_categories": ["sniper", "assassin", "thrower"],
-        "range_rating": (693-72)/693    #andel tiles som är öppen map eller buskar
-    }
-]
-
+class Map:
+    def __init__(self, map_name, mode, range_rating):
+        self.map_name = map_name
+        self.mode = mode
+        self.range_rating = range_rating
 
 class Brawler:
-    def __init__(self, brawl_name, categories, range, hp):
+    def __init__(self, brawl_name, categories, range, hp, play_pos, water_playable):
         self.brawl_name = brawl_name
         self.categories = categories
         self.range = range
         self.hp = hp
+        self.play_pos = play_pos
+        self.water_playable = water_playable
 
-
-
-'''
-brawlers = [
-    {
-        "name": "shelly",
-        "categories": ["tank_counter/DPS", "tank"],
-        "range": "short"
-    },
-    {
-        "name": "nita",
-        "categories": ["tank_counter/DPS"],
-        "range": "mid"
-    },
-    {
-        "name": "colt",
-        "categories": ["sniper", "tank_counter/DPS"],
-        "range": "long"
-    },
-    {
-        "name": "bull",
-        "categories": ["tank"],
-        "range": "short"
-    }
-]'''
-
-#en typ av datastruktur, jobbigt med lista pga ordnade element?
-brawlers = [
-            Brawler("tick", ["thrower"], "long", 2200),
-            Brawler("frank", ["tank"], "short", 6700),
-            Brawler("barley",["thrower"], "mid", 2400),
-            Brawler("brock", ["sniper", "tank counter/DPS"], "long", 2400),
-            Brawler("byron", ["sniper", "support"], "long", 2400),
-            Brawler("crow", ["assassin"], "mid", 2400),
-            Brawler("nani", ["sniper", "tank counter/DPS"], "long", 2400),
-            Brawler("piper", ["sniper", "tank counter/DPS"], "long", 2300),
-            Brawler("primo", ["tank"], "short", 6000),
-            Brawler("draco", ["tank"], "short", 5500)
-            ]
-
-
-#bättre med dict?
-brawlers_test = {
-    "tick": Brawler("TICK", ["thrower"], "long", 2200),
-    "frank": Brawler("FRANK", ["tank"], "short", 6700),
-    "byron": Brawler("BYRON",["sniper", "support"], "long", 2400)
+brawlers = {
+    "tick": Brawler("TICK", ["thrower"], "long", 2200, ["mid","lane"], False),
+    "frank": Brawler("FRANK", ["tank"], "short", 6700, ["lane"], False),
+    "byron": Brawler("BYRON",["sniper", "support"], "long", 2400, ["mid", "lane"], False),
+    "angelo": Brawler("ANGELO", ["sniper"], "long", 3000, ["mid", "lane"], True),
+    "piper": Brawler("PIPER", ["sniper", "DPS"], "long", 2300, ["mid", "lane"], False),
+    "primo": Brawler("EL PRIMO", ["tank"], "short", 6000, ["lane"], False),
+    "nani": Brawler("NANI", ["sniper", "DPS"], "long", 2400, ["mid", "lane"], False),
+    "draco": Brawler("DRACO", ["tank"], "short", 5500, ["lane"], False)
 }
 
-print(brawlers_test["tick"].hp)
-print(brawlers[9].brawl_name)
+maps = {
+    "snake prairie": Map("Snake Prairie", "bounty", None),
+    "tripple dribble": Map("Tripple Dribble", "brawl ball", None),
+}
+
+
+def lowest_hp(brawler_list):
+    hp_list = [brawlers[brawler].hp for brawler in brawler_list]
+    return min(hp_list)
+
+def highest_hp(brawler_list):
+    hp_list = [brawlers[brawler].hp for brawler in brawler_list]
+    return max(hp_list)
+
+print(highest_hp(["tick", "piper", "frank"]))
+
 
 def hp_rating(brawler_list):        #räknar alla hp som lvl 1 brawlers
     if len(brawler_list) == 1:
-        min_sum = brawlers[0].hp                        #tick (lägst)
-        max_sum = brawlers[1].hp                        #franks hp (högst)
+        min_sum = brawlers["tick"].hp                        #tick (minst)
+        max_sum = brawlers["frank"].hp                        #franks hp (störst)
         #skala in värdena:
     elif len(brawler_list) == 2:
-        minsum = brawlers[0].hp + brawlers[7].hp        #ticks + pipers hp
-        maxsum = brawlers[1].hp + brawlers[8].hp        #franks + primos hp
+        minsum = brawlers["tick"].hp + brawlers["piper"].hp         #minst hp
+        maxsum = brawlers["frank"].hp + brawlers["primo"].hp        #störst hp
     elif len(brawler_list) == 3:
-        sum_min = brawlers[0].hp + brawlers[7].hp + brawlers[6].hp  #ticks + pipers + nanis hp
-        sum_max = brawlers[1].hp + brawlers[8].hp + brawlers[9].hp      #franks + primos + dracos hp (högst o fallande)
+        sum_min = brawlers["tick"].hp + brawlers["piper"].hp + brawlers["nani"].hp              #minst hp
+        sum_max = brawlers["frank"].hp + brawlers["primo"].hp + brawlers["draco"].hp            #störst hp
